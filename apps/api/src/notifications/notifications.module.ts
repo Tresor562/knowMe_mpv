84ac19ed3e7ccb32aa185fef20d6c86279688110
Tresor562 <hontonnoutresor@gmail.com>
2026-08-02 00:19:41 +0100

    fix(api): defer notifications realtime module dependency

--- apps/api/src/notifications/notifications.module.ts
+++ apps/api/src/notifications/notifications.module.ts
@@ -1,11 +1,11 @@
-import { Global, Module } from '@nestjs/common';
+import { forwardRef, Global, Module } from '@nestjs/common';
 import { RealtimeModule } from '../realtime/realtime.module';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Global()
 @Module({
-  imports: [RealtimeModule],
+  imports: [forwardRef(() => RealtimeModule)],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService]
