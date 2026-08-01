1630c75966191c573c69dc0a9fa7bc8229f72b29
Tresor562 <hontonnoutresor@gmail.com>
2026-08-01 17:46:35 +0100

    feat(account): include verification privacy lifecycle

--- apps/api/src/account/account.module.ts
+++ apps/api/src/account/account.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { VerificationModule } from '../verification/verification.module';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 
 @Module({
+  imports: [VerificationModule],
   controllers: [AccountController],
   providers: [AccountService]
 })
