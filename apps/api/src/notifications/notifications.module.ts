21653e7fd18eb9e304f96f8e5225f0d4f4323a73
Tresor562 <hontonnoutresor@gmail.com>
2026-08-01 10:56:53 +0100

    refactor(notifications): expose a global realtime notification service

--- apps/api/src/notifications/notifications.module.ts
+++ apps/api/src/notifications/notifications.module.ts
@@ -1,6 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { RealtimeModule } from '../realtime/realtime.module';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
-@Module({ controllers: [NotificationsController], providers: [NotificationsService] })
+@Global()
+@Module({
+  imports: [RealtimeModule],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
+  exports: [NotificationsService]
+})
 export class NotificationsModule {}
