e0830e07fb57c70c1f895c8056777cffc8e02c86
Tresor562 <hontonnoutresor@gmail.com>
2026-08-02 09:11:38 +0100

    feat(progression): connect XP service to challenges

--- apps/api/src/challenges/challenges.module.ts
+++ apps/api/src/challenges/challenges.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ProgressionModule } from '../progression/progression.module';
 import { ChallengeResultsService } from './challenge-results.service';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
 
 @Module({
+  imports: [ProgressionModule],
   controllers: [ChallengesController],
   providers: [ChallengesService, ChallengeResultsService],
   exports: [ChallengeResultsService]
