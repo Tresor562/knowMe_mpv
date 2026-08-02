6aa548de1b6b5d4a9df5b2125f3a22d03d3e0576
Tresor562 <hontonnoutresor@gmail.com>
2026-08-02 11:41:15 +0100

    feat(concept-k): register asset catalog services

--- apps/api/src/concept-k/concept-k.module.ts
+++ apps/api/src/concept-k/concept-k.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import {
+  AdminConceptKAssetsController,
+  ConceptKAssetsController
+} from './concept-k-assets.controller';
+import { ConceptKAssetsService } from './concept-k-assets.service';
 import { ConceptKController } from './concept-k.controller';
 import { ConceptKService } from './concept-k.service';
 
 @Module({
-  controllers: [ConceptKController],
-  providers: [ConceptKService],
-  exports: [ConceptKService]
+  controllers: [
+    ConceptKController,
+    ConceptKAssetsController,
+    AdminConceptKAssetsController
+  ],
+  providers: [ConceptKService, ConceptKAssetsService],
+  exports: [ConceptKService, ConceptKAssetsService]
 })
 export class ConceptKModule {}
