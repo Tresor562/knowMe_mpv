d8c5f16bbf92a85281282dcddfbfad0f513d7894
Tresor562 <hontonnoutresor@gmail.com>
2026-08-02 16:27:18 +0100

    feat(kmd-028): register cosmetic shop services

--- apps/api/src/cosmetics/cosmetics.module.ts
+++ apps/api/src/cosmetics/cosmetics.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import {
+  AdminCosmeticsShopController,
+  CosmeticsShopController
+} from './cosmetics-shop.controller';
+import { CosmeticsShopService } from './cosmetics-shop.service';
 import { AdminCosmeticsController, CosmeticsController } from './cosmetics.controller';
 import { CosmeticsService } from './cosmetics.service';
 
 @Module({
-  controllers: [CosmeticsController, AdminCosmeticsController],
-  providers: [CosmeticsService],
-  exports: [CosmeticsService]
+  controllers: [
+    CosmeticsController,
+    AdminCosmeticsController,
+    CosmeticsShopController,
+    AdminCosmeticsShopController
+  ],
+  providers: [CosmeticsService, CosmeticsShopService],
+  exports: [CosmeticsService, CosmeticsShopService]
 })
 export class CosmeticsModule {}
