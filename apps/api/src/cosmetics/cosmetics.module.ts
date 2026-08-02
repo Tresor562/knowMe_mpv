fa80828b8811125dd995295c295e5a16440a0982
Tresor562 <hontonnoutresor@gmail.com>
2026-08-02 14:54:07 +0100

    feat(kmd-027): register cosmetics module

---
+++ apps/api/src/cosmetics/cosmetics.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import {
+  AdminCosmeticsController,
+  CosmeticsController
+} from './cosmetics.controller';
+import { CosmeticsService } from './cosmetics.service';
+
+@Module({
+  controllers: [CosmeticsController, AdminCosmeticsController],
+  providers: [CosmeticsService],
+  exports: [CosmeticsService]
+})
+export class CosmeticsModule {}
