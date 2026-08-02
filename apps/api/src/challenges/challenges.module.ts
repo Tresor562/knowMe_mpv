973e47ab173ad5372e489e06d7c3af382ba6bb36
Tresor562 <hontonnoutresor@gmail.com>
2026-08-02 08:48:34 +0100

    feat: register challenge result service

--- apps/api/src/challenges/challenges.module.ts
+++ apps/api/src/challenges/challenges.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ChallengeResultsService } from './challenge-results.service';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
 
-@Module({ controllers:[ChallengesController], providers:[ChallengesService] })
+@Module({
+  controllers: [ChallengesController],
+  providers: [ChallengesService, ChallengeResultsService],
+  exports: [ChallengeResultsService]
+})
 export class ChallengesModule {}
