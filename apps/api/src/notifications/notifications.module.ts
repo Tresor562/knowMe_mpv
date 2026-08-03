64b4f34731d691768e6e9de5c41d121424d8f5ba
Tresor562 <hontonnoutresor@gmail.com>
2026-08-03 22:51:56 +0100

    feat(kmd-048): wire intelligent notification center

--- apps/api/src/notifications/notifications.module.ts
+++ apps/api/src/notifications/notifications.module.ts
@@ -1,13 +1,32 @@
 import { forwardRef, Global, Module } from '@nestjs/common';
 import { RealtimeModule } from '../realtime/realtime.module';
+import { NotificationCenterDigestSchedulerService } from './notification-center-digest-scheduler.service';
+import { NotificationCenterDigestService } from './notification-center-digest.service';
+import { AdminNotificationCenterOperationsController } from './notification-center-operations.controller';
+import { NotificationCenterPolicyService } from './notification-center-policy.service';
+import { NotificationCenterService } from './notification-center.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Global()
 @Module({
   imports: [forwardRef(() => RealtimeModule)],
-  controllers: [NotificationsController],
-  providers: [NotificationsService],
-  exports: [NotificationsService]
+  controllers: [
+    NotificationsController,
+    AdminNotificationCenterOperationsController
+  ],
+  providers: [
+    NotificationsService,
+    NotificationCenterPolicyService,
+    NotificationCenterService,
+    NotificationCenterDigestService,
+    NotificationCenterDigestSchedulerService
+  ],
+  exports: [
+    NotificationsService,
+    NotificationCenterPolicyService,
+    NotificationCenterService,
+    NotificationCenterDigestService
+  ]
 })
 export class NotificationsModule {}
