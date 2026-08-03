28c9cb8a704691dfa98c8a27cdb69a9982733d5c
Tresor562 <hontonnoutresor@gmail.com>
2026-08-03 23:42:23 +0100

    feat(kmd-049): validate locale preference updates

---
+++ apps/api/src/i18n/dto/update-locale-preference.dto.ts
@@ -0,0 +1,11 @@
+import { IsIn, IsInt, Min } from 'class-validator';
+import { SUPPORTED_LOCALES, SupportedLocale } from '@knowme/i18n-contract';
+
+export class UpdateLocalePreferenceDto {
+  @IsIn(SUPPORTED_LOCALES)
+  locale!: SupportedLocale;
+
+  @IsInt()
+  @Min(0)
+  expectedVersion!: number;
+}
