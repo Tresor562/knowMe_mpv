54758895d8f7de8c9a4908299724ab58d131d68c
Tresor562 <hontonnoutresor@gmail.com>
2026-08-04 04:06:53 +0100

    feat(kmd-052): connect game lifecycle to accounts

--- apps/api/src/account/account.module.ts
+++ apps/api/src/account/account.module.ts
@@ -3,6 +3,7 @@ import { AppearanceModule } from '../appearance/appearance.module';
 import { ConceptKModule } from '../concept-k/concept-k.module';
 import { CosmeticsModule } from '../cosmetics/cosmetics.module';
 import { CreatorsModule } from '../creators/creators.module';
+import { GamePlatformModule } from '../games/game-platform.module';
 import { MediaModule } from '../media/media.module';
 import { PrivacyModule } from '../privacy/privacy.module';
 import { SecurityModule } from '../security/security.module';
@@ -16,6 +17,7 @@ import { AccountService } from './account.service';
     PrivacyModule,
     MediaModule,
     CreatorsModule,
+    GamePlatformModule,
     ConceptKModule,
     CosmeticsModule,
     AppearanceModule,
