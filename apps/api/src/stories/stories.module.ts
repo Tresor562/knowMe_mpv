1f61d1502ec8f51a7eac052911eeb8e1866a96cf
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-09-06 19:12:40 +0100

    feat(stories): register Story discovery

--- apps/api/src/stories/stories.module.ts
+++ apps/api/src/stories/stories.module.ts
@@ -3,14 +3,16 @@ import { EntitlementsModule } from '../entitlements/entitlements.module';
 import { ModerationModule } from '../moderation/moderation.module';
 import { ObservabilityModule } from '../observability/observability.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StoryDiscoveryController } from './story-discovery.controller';
+import { StoryDiscoveryService } from './story-discovery.service';
 import { StoriesController } from './stories.controller';
 import { StoriesPremiumBootstrap } from './stories-premium.bootstrap';
 import { StoriesService } from './stories.service';
 
 @Module({
   imports: [PrismaModule, EntitlementsModule, ModerationModule, ObservabilityModule],
-  controllers: [StoriesController],
-  providers: [StoriesService, StoriesPremiumBootstrap],
-  exports: [StoriesService]
+  controllers: [StoriesController, StoryDiscoveryController],
+  providers: [StoriesService, StoryDiscoveryService, StoriesPremiumBootstrap],
+  exports: [StoriesService, StoryDiscoveryService]
 })
 export class StoriesModule {}
