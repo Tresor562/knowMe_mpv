ada8cf480bfc4b47c97bc88fc66f42c97e5418b4
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-09-06 19:02:27 +0100

    feat(stories): register stories module

---
+++ apps/api/src/stories/stories.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { EntitlementsModule } from '../entitlements/entitlements.module';
+import { ModerationModule } from '../moderation/moderation.module';
+import { ObservabilityModule } from '../observability/observability.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { StoriesController } from './stories.controller';
+import { StoriesService } from './stories.service';
+
+@Module({
+  imports: [PrismaModule, EntitlementsModule, ModerationModule, ObservabilityModule],
+  controllers: [StoriesController],
+  providers: [StoriesService],
+  exports: [StoriesService]
+})
+export class StoriesModule {}
