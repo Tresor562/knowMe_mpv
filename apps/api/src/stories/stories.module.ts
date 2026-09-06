0707ed26c012c2d96478692969c23e2d46cde055
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-09-06 19:17:05 +0100

    feat(stories): wire secure Story media streaming

--- apps/api/src/stories/stories.module.ts
+++ apps/api/src/stories/stories.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { EntitlementsModule } from '../entitlements/entitlements.module';
+import { MediaModule } from '../media/media.module';
 import { ModerationModule } from '../moderation/moderation.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { ObservabilityModule } from '../observability/observability.module';
@@ -10,6 +11,8 @@ import { StoryDiscoveryController } from './story-discovery.controller';
 import { StoryDiscoveryService } from './story-discovery.service';
 import { StoryInteractionNotifier } from './story-interaction-notifier.service';
 import { StoryLifecycleService } from './story-lifecycle.service';
+import { StoryMediaController } from './story-media.controller';
+import { StoryMediaService } from './story-media.service';
 import { StoriesController } from './stories.controller';
 import { StoriesPremiumBootstrap } from './stories-premium.bootstrap';
 import { StoriesService } from './stories.service';
@@ -18,13 +21,20 @@ import { StoriesService } from './stories.service';
   imports: [
     PrismaModule,
     EntitlementsModule,
+    MediaModule,
     ModerationModule,
     NotificationsModule,
     ObservabilityModule
   ],
-  controllers: [StoriesController, StoryDiscoveryController, StoryAlbumsController],
+  controllers: [
+    StoriesController,
+    StoryMediaController,
+    StoryDiscoveryController,
+    StoryAlbumsController
+  ],
   providers: [
     StoriesService,
+    StoryMediaService,
     StoryAlbumsService,
     StoryDiscoveryService,
     StoryInteractionNotifier,
