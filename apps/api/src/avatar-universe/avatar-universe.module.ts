7b5f289bd8b164839241cb1e5a5c0657f91a2f8f
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-09-17 18:59:12 +0100

    feat(avatar): wire server-authoritative Avatar DNA service

--- apps/api/src/avatar-universe/avatar-universe.module.ts
+++ apps/api/src/avatar-universe/avatar-universe.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AvatarUniverseController } from './avatar-universe.controller';
+import { AvatarDnaService } from './avatar-dna.service';
 
 @Module({
-  controllers: [AvatarUniverseController]
+  controllers: [AvatarUniverseController],
+  providers: [AvatarDnaService],
+  exports: [AvatarDnaService]
 })
 export class AvatarUniverseModule {}
