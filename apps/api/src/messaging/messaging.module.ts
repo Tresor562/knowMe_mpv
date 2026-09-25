8add921763eb84644c209fe0307c0aa5eb550782
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-09-25 21:46:08 +0100

    feat(messenger): register server voice transformation

--- apps/api/src/messaging/messaging.module.ts
+++ apps/api/src/messaging/messaging.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MediaModule } from '../media/media.module';
 import { RealtimeModule } from '../realtime/realtime.module';
 import { ConversationTranslationService } from './conversation-translation.service';
 import { MediaMessageTokenService } from './media-message-token.service';
@@ -7,16 +8,18 @@ import { MessagingController } from './messaging.controller';
 import { MessagingService } from './messaging.service';
 import { StickerController } from './stickers/sticker.controller';
 import { StickerTokenService } from './stickers/sticker-token.service';
+import { VoiceTransformService } from './voice-transform.service';
 
 @Module({
-  imports: [RealtimeModule],
+  imports: [RealtimeModule, MediaModule],
   controllers: [MessagingController, StickerController],
   providers: [
     MessagingService,
     MessageEditingService,
     StickerTokenService,
     MediaMessageTokenService,
-    ConversationTranslationService
+    ConversationTranslationService,
+    VoiceTransformService
   ],
   exports: [MessagingService, StickerTokenService, MediaMessageTokenService]
 })
