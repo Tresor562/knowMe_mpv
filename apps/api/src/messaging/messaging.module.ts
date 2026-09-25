50c8b498aec8049af1de76d1e4f4543eaf3f20a0
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-09-25 21:30:12 +0100

    feat(messenger): register media and translation services

--- apps/api/src/messaging/messaging.module.ts
+++ apps/api/src/messaging/messaging.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RealtimeModule } from '../realtime/realtime.module';
+import { ConversationTranslationService } from './conversation-translation.service';
+import { MediaMessageTokenService } from './media-message-token.service';
 import { MessageEditingService } from './message-editing.service';
 import { MessagingController } from './messaging.controller';
 import { MessagingService } from './messaging.service';
@@ -9,7 +11,13 @@ import { StickerTokenService } from './stickers/sticker-token.service';
 @Module({
   imports: [RealtimeModule],
   controllers: [MessagingController, StickerController],
-  providers: [MessagingService, MessageEditingService, StickerTokenService],
-  exports: [MessagingService, StickerTokenService]
+  providers: [
+    MessagingService,
+    MessageEditingService,
+    StickerTokenService,
+    MediaMessageTokenService,
+    ConversationTranslationService
+  ],
+  exports: [MessagingService, StickerTokenService, MediaMessageTokenService]
 })
 export class MessagingModule {}
